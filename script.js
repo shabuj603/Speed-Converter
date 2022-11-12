25dcfffae6d36ac9e59ab0664c17f89f40f0672c
shabuj603 <72185409+shabuj603@users.noreply.github.com>
2022-11-12 11:15:07 +0600

    side_nav_bar

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// navbar section start 
+function navFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+
+ // Load google charts
+ google.charts.load('current', {'packages':['corechart']});
+ google.charts.setOnLoadCallback(drawChart);
+
+ // Draw the chart and set the chart values
+ function drawChart() {
+   var data = google.visualization.arrayToDataTable([
+   ['Task', 'Hours per Day'],
+   ['Work', 8],
+   ['Eat', 2],
+   ['TV', 4],
+   ['Gym', 4],
+   ['Sleep', 8]
+ ]);
+ 
+   // Optional; add a title and set the width and height of the chart
+   var options = {'title':'My Average Day', 'width':550, 'height':400};
+ 
+   // Display the chart inside the <div> element with id="piechart"
+   var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+   chart.draw(data, options);
+ }
+// piechart end 
 
 function speedConverter(valNum){
    document.getElementById("outputKPH").innerHTML=valNum*1.609344;
@@ -73,3 +108,18 @@ function lengthConverter(source,valNum){
         inputKilometers.value=(valNum/0.62137).toFixed(2);    
       }
 }
+
+// sideNav start
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    // document.getElementById("myNav").style.width = "250px";
+    // document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    // document.getElementById("myNav").style.marginLeft = "0%";
+  }
+  // sideNav start end
+  
